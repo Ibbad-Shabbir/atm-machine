4040df6e3aeb3dbd47fbdad9a455fe01751a5ddd
Ibbad Shabbir <Ibbadshabbir02@gmail.com>
2024-03-25 20:02:51 +0500

    ATM done

---
+++ index.ts
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answers = await inquirer.prompt([
+  {
+    type: "input",
+    name: "cardnum",
+    message: "Enter your card number",
+  },
+  {
+    type: "password",
+    name: "cvv",
+    message: "Enter your CVV",
+  },
+  {
+    type: "list",
+    name: "transactionType",
+    choices: ["Deposit", "Withdrawl"],
+    message: "What type of transaction would you like to create?",
+  },
+  {
+    type: "number",
+    name: "amount",
+    message: `enter amount your desired amount: `,
+    when(answers) {
+      return answers.transactionType == "Deposit";
+    },
+  },
+  {
+    type: "number",
+    name: "amount",
+    message: `enter amount your desired amount: `,
+    when(answers) {
+      return answers.transactionType == "Withdrawl";
+    },
+  },
+]);
+
+if (answers.transactionType === "Withdrawl") {
+  const balance = 500000;
+  console.log("Previous Balance was: ", balance);
+
+  const withdraw_amount = answers.amount;
+  if (balance >= withdraw_amount) {
+    const remainingamount = balance - withdraw_amount;
+    console.log("Remaining Balance is: ", remainingamount);
+  } else {
+    console.log("Insufficient Funds");
+  }
+} else {
+  const balance = 500000;
+  console.log("Previous Balance was: ", balance);
+
+  const deposit_amount = answers.amount;
+  if (balance <= deposit_amount || balance >= deposit_amount) {
+    const new_amount = balance + deposit_amount;
+    console.log("Remaining Balance is: ", new_amount);
+  }
+}
