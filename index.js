bf1474ae876157e1ab9a1ce1112d33ea0e16898c
Ibbad Shabbir <Ibbadshabbir02@gmail.com>
2024-03-27 00:35:03 +0500

    Major UPDATE!!!
    added login and registering functionalities

--- index.js
+++ index.js
@@ -1,57 +1,109 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const answers = await inquirer.prompt([
+const initial = await inquirer.prompt([
     {
         type: "input",
-        name: "cardnum",
-        message: "Enter your card number",
-    },
-    {
-        type: "password",
-        name: "cvv",
-        message: "Enter your CVV",
-    },
-    {
-        type: "list",
-        name: "transactionType",
-        choices: ["Deposit", "Withdrawl"],
-        message: "What type of transaction would you like to create?",
-    },
-    {
-        type: "number",
-        name: "amount",
-        message: `enter your desired amount (remaining balance is 500,000): `,
-        when(answers) {
-            return answers.transactionType == "Deposit";
-        },
-    },
-    {
-        type: "number",
-        name: "amount",
-        message: `enter your desired amount (remaining balance is 500,000): `,
-        when(answers) {
-            return answers.transactionType == "Withdrawl";
-        },
+        message: "What is your name?",
+        name: "name",
     },
 ]);
-if (answers.transactionType === "Withdrawl") {
-    const balance = 500000;
-    console.log("Previous Balance was: ", balance);
-    const withdraw_amount = answers.amount;
-    if (balance >= withdraw_amount) {
-        const remainingamount = balance - withdraw_amount;
-        console.log("Remaining Balance is: ", remainingamount);
-    }
-    else {
-        console.log("Insufficient Funds");
-    }
+if (initial.name) {
+    console.log(`Hello ${initial.name}!`);
 }
 else {
-    const balance = 500000;
-    console.log("Previous Balance was: ", balance);
-    const deposit_amount = answers.amount;
-    if (balance <= deposit_amount || balance >= deposit_amount) {
-        const new_amount = balance + deposit_amount;
-        console.log("Remaining Balance is: ", new_amount);
+    console.log("please enter your name");
+}
+const initial2 = await inquirer.prompt([
+    {
+        type: "list",
+        name: "choice",
+        message: "how can i assist you today?",
+        choices: ["Register", "Login"],
+    },
+]);
+if (initial2.choice === "Register") {
+    const register = await inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter your name",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "Enter your card number",
+            name: "card",
+        },
+        {
+            type: "password",
+            message: "Enter your password",
+            name: "password",
+        },
+        {
+            type: "password",
+            message: "Confirm your password",
+            name: "confirm",
+        },
+    ]);
+    if (register.confirm !== register.password) {
+        console.error("passwords do not match!");
+    }
+    else {
+        console.log("your account has been created successfully, please run `npx ibbad-atm` to login in your account");
+    }
+}
+else if (initial2.choice === "Login") {
+    const login = await inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter your card number",
+            name: "card",
+        },
+        {
+            type: "password",
+            message: "Enter your password",
+            name: "password",
+        },
+        {
+            type: "list",
+            message: "What would you like to do today?",
+            name: "choice",
+            choices: ["Withdrawl", "Deposit", "Check Balance"],
+        },
+        {
+            type: "number",
+            message: "Enter the amount you would like to withdraw",
+            name: "amount",
+            when(answers) {
+                return answers.amount = "Withdrawl";
+            },
+        },
+        {
+            type: "number",
+            message: "Enter the amount you would like to deposit",
+            name: "amount",
+            when(answers) {
+                return answers.amount = "Deposit";
+            },
+        },
+    ]);
+    if (login.choice === "Check Balance") {
+        console.log("your balance is $50,000");
+    }
+    else if (login.choice === "Withdrawl") {
+        const balance = 50000;
+        const givenAmount = login.amount;
+        if (balance >= givenAmount) {
+            console.log("Transaction was Successfull\n your current balance is", balance - givenAmount);
+        }
+        else {
+            console.log("Transaction failed, Insufficient Funds");
+        }
+    }
+    else {
+        const balance = 50000;
+        const givenAmount = login.amount;
+        if (balance <= givenAmount || balance >= givenAmount) {
+            console.log("Transaction was Successfull\n your current balance is", balance + givenAmount);
+        }
     }
 }
